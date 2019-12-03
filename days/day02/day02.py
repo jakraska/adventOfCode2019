5214534acadb2935015b0429214f79d8b8f799bd
Jonathan Kraska <jkraska@appnexus.com>
2019-12-02 22:34:16 -0500

    Refactored Day2

--- days/day02/day02.py
+++ days/day02/day02.py
@@ -1,43 +1,33 @@
 import utils.input
 
 
-def part1(codes):
-    codes[1] = 12
-    codes[2] = 2
-    run_program(codes)
-    print(codes[0])
+def part1():
+    print(run_program(utils.input.getInputAsCSVInts(), 12, 2))
 
 
 def part2():
-
     for i in range(100):
         for j in range(100):
             codes = utils.input.getInputAsCSVInts()
-            codes[1] = i
-            codes[2] = j
-            run_program(codes)
-            if codes[0] == 19690720:
+            if run_program(codes, i, j) == 19690720:
                 print("success")
                 print(100 * i + j)
                 return
 
-def run_program(codes):
+
+def run_program(codes, noun, verb):
     i = 0
+    codes[1] = noun
+    codes[2] = verb
     while codes[i] != 99:
         a = codes[codes[i + 1]]
         b = codes[codes[i + 2]]
         newVal = (a + b) if codes[i] == 1 else (a * b)
         codes[codes[i + 3]] = newVal
         i += 4
-    return codes
+    return codes[0]
 
-# def part2(input):
-
-
-# codes = utils.input.getInputAsCSVInts()
-# part1(codes)
 
 part2()
 
-
-#part1(utils.input.getInputAsInts())
+#part1()
